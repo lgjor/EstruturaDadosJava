65a5d37d1d00864590941cfc3adc1b5675584ca7
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-06-05 14:32:14 -0300

    Adicionado implementação de queue fila em Java por meio da linkedlist

---
+++ src/main/java/com/projeto/Queue/Main.java
@@ -0,0 +1,32 @@
+package com.projeto.Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Queue<Carro> queueCarros = new LinkedList<>();
+
+        queueCarros.add(new Carro("Ford"));
+        queueCarros.add(new Carro("Chevrolet"));
+        queueCarros.add(new Carro("Fiat"));
+
+        // True - conseguiu adicionar, se não conseguir lança um erro
+        System.out.println(queueCarros.add(new Carro("Peugeot")));
+
+        // True - conseguiu adicionar, se não conseguir, nada acontece
+        System.out.println(queueCarros.offer(new Carro("Renault")));
+
+        // Retorna o primeiro elemento da fila, se estiver vazio, retorna null
+        System.out.println(queueCarros.peek());
+
+        // Retorna o primeiro elemento da fila e remove da fila
+        System.out.println(queueCarros.poll());
+
+        System.out.println(queueCarros.isEmpty());
+
+        System.out.println(queueCarros.size());
+    }
+}
